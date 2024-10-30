8b361bd54ce452381080a4035bc6984dc61c3b7e
Kwan <kwanhokwon63@gmail.com>
2024-10-30 18:29:46 +0900

    finish

--- lib.js
+++ lib.js
@@ -3,11 +3,29 @@ function avg(numbers) {
     return s / numbers.length;
 }
 function prime(num) {
-    return false;
+    if (num < 2) {
+        return false;
+    }
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
+
 function factorial(num) {
-    return -1;
+    if (num < 0)
+        return -1;
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
 }
+
 module.exports = {
     avg,
     prime,
